da5b05966a0fb91eaab751ae89cd6f5343e40027
Zhenya1975 <zhenya1975@mail.ru>
2022-10-19 12:34:26 +0600

    init

--- project/routes/routes.py
+++ project/routes/routes.py
@@ -20,6 +20,52 @@ home = Blueprint('home', __name__, template_folder='templates')
 
 socketio = extensions.socketio
 
+def update_league_and_team_data(driver, country, league_name, sport_id):
+    # проверяем есть ли эта лига в базе
+    league_data = LeagueDB.query.filter_by(league_country=country, league_name=league_name).first()
+    league_id = league_data.league_id
+    if league_data:
+        print("Данные о лиге уже есть в базе")
+        pass  # если лига есть, то просто пропускаем
+    else:
+        # создаем новую лигу
+        print("Данных о лиге в нет в базе")
+        last_created_league_id = new_league(country, league_name, sport_id)
+        league_id = last_created_league_id
+        # переходим в список команд для заполнения
+        driver.find_element("xpath", "//span[@class='event__title--name']").click()
+        driver.implicitly_wait(10)
+        driver.find_element("xpath", "//a[@class='tabs__tab standings_table']").click()
+
+        # получаем список команд
+        teams = driver.find_elements("xpath", "//a[@class='tableCellParticipant__name']")
+        for team in teams:
+            team_name = team.text
+            # проверяем есть ли уже запись о команде
+            team_data = TeamsDB.query.filter_by(league_id=league_id, team_name=team_name).first()
+            if team_data:
+                pass
+            else:
+                new_team_record = TeamsDB(
+                    league_id=league_id,
+                    team_name=team_name
+                )
+                db.session.add(new_team_record)
+                db.session.commit()
+                last_created_team = TeamsDB.query.order_by(desc(TeamsDB.team_id)).first()
+                print("Команда : ", last_created_team.team_name, " была создана")
+        # возвращаемся обратно
+        driver.execute_script("window.history.go(-1)")
+        driver.implicitly_wait(3)
+        driver.execute_script("window.history.go(-1)")
+        driver.implicitly_wait(10)
+        tabs = driver.find_elements("xpath", "//div[@class='filters__text filters__text--default']")
+        for tab in tabs:
+            tab_name = tab.text
+            if tab_name.lower() == "live":
+                tab.click()
+
+
 
 def new_league(country, league_name, sport_id):
     new_league = LeagueDB(
@@ -39,13 +85,58 @@ def new_league(country, league_name, sport_id):
 def home_view():
     return render_template('home.html')
 
+@home.route('/parse_finished')
+def parse_finished():
+    driver = webdriver.Chrome("/usr/local/bin/chromedriver")
+    driver.get("https://www.flashscorekz.com/basketball/")
+    # driver.get("https://www.flashscorekz.com/hockey/")
+    sport_id = 1
+    top_header_data = {}
+    driver.implicitly_wait(10)
+    # жмем на кнопку принять куки.
+    try:
+        driver.find_element("id", 'onetrust-accept-btn-handler').click()
+    except Exception as e:
+        print("accept_all_button exception ", e)
+    tabs = driver.find_elements("xpath", "//div[@class='filters__text filters__text--default']")
+    for tab in tabs:
+        tab_name = tab.text
+        if tab_name.lower() == "завершенные":
+            tab.click()
+
+    # получаем топ хедеры
+    try:
+        # top_headers_list = driver.find_elements("xpath", "//div[@class='event__header top']")
+        top_headers_list = driver.find_elements("xpath", "//div[@class='event__titleBox']")
+        for top_header in top_headers_list:
+            # проверяем есть ли у топ хедера сохраненная лига и команды
+            # print(top_header)
+            # country_top_header = top_header.find_element("xpath", "//span[@class='event__title--type']")
+            country_top_header = top_header.find_element(By.CLASS_NAME, "event__title--type")
+            # league_top_header = top_header.find_element("xpath", "//span[@class='event__title--name']")
+            league_top_header = top_header.find_element(By.CLASS_NAME, "event__title--name")
+            country_top_header = country_top_header.text.lower()
+            league_top_header = league_top_header.text
+            print("На странице есть топ-хедер. Страна: ", country_top_header, ", лига: ", league_top_header)
+
+        return redirect(url_for('home.home_view'))
+
+    except Exception as e:
+        print("Ошибка при попытке получения топ-хедеров в завершенных: ", e)
+        return redirect(url_for('home.home_view'))
+
+
+
+
+
 
 @home.route('/parse_live')
 def parse_live():
     driver = webdriver.Chrome("/usr/local/bin/chromedriver")
-    # driver.get("https://www.flashscorekz.com/basketball/")
-    driver.get("https://www.flashscorekz.com/hockey/")
-    sport_id = 2
+    driver.get("https://www.flashscorekz.com/basketball/")
+    # driver.get("https://www.flashscorekz.com/hockey/")
+    sport_id = 1
+    top_header_data = {}
 
     driver.implicitly_wait(10)
     # жмем на кнопку принять куки.
@@ -58,42 +149,42 @@ def parse_live():
         tab_name = tab.text
         if tab_name.lower() == "live":
             tab.click()
-    # получаем страну и лигу
-    top_header = driver.find_element("xpath", "//div[@class='event__header top']")
-    # получаем страну и лигу из топхедера
-    # это будет первое вхождение, если живые матчи есть на странице
-
+    # получаем топ хедер
     try:
+        top_header = driver.find_element("xpath", "//div[@class='event__header top']")
+        # проверяем есть ли у топ хедера сохраненная лига и команды
         country_top_header = driver.find_element("xpath", "//span[@class='event__title--type']")
         league_top_header = driver.find_element("xpath", "//span[@class='event__title--name']")
-
         country_top_header = country_top_header.text.lower()
-        league_top_header =league_top_header.text
+        league_top_header = league_top_header.text
+        print("На странице есть топ-хедер. Страна: ", country_top_header, ", лига: ", league_top_header)
         # проверяем есть ли эта лига в базе
         league_data = LeagueDB.query.filter_by(league_country=country_top_header, league_name=league_top_header).first()
+        top_header_league_id = league_data.league_id
         if league_data:
-            pass
+            print("Данные о лиге в топ-хедере уже есть в базе")
+            pass # если лига есть, то просто пропускем
         else:
             # создаем новую лигу
+            print("Данных о лиге в топ-хедере нет в базе")
             last_created_league_id = new_league(country_top_header, league_top_header, sport_id)
-            league_id = last_created_league_id
-            #  и переходим в список команд для заполнения
+            top_header_league_id = last_created_league_id
+            # переходим в список команд для заполнения
             driver.find_element("xpath", "//span[@class='event__title--name']").click()
             driver.implicitly_wait(10)
             driver.find_element("xpath", "//a[@class='tabs__tab standings_table']").click()
-            # парсим таблицу с командами
 
             # получаем список команд
             teams = driver.find_elements("xpath", "//a[@class='tableCellParticipant__name']")
             for team in teams:
                 team_name = team.text
                 # проверяем есть ли уже запись о команде
-                team_data = TeamsDB.query.filter_by(league_id=league_id, team_name=team_name).first()
+                team_data = TeamsDB.query.filter_by(league_id=top_header_league_id, team_name=team_name).first()
                 if team_data:
                     pass
                 else:
                     new_team_record = TeamsDB(
-                        league_id=league_id,
+                        league_id=top_header_league_id,
                         team_name=team_name
                     )
                     db.session.add(new_team_record)
@@ -101,35 +192,67 @@ def parse_live():
                     last_created_team = TeamsDB.query.order_by(desc(TeamsDB.team_id)).first()
                     print("Команда : ", last_created_team.team_name, " была создана")
             # возвращаемся обратно
+            driver.execute_script("window.history.go(-1)")
+            driver.execute_script("window.history.go(-1)")
+            driver.implicitly_wait(10)
+            tabs = driver.find_elements("xpath", "//div[@class='filters__text filters__text--default']")
+            for tab in tabs:
+                tab_name = tab.text
+                if tab_name.lower() == "live":
+                    tab.click()
+        # получаем матчи в топ-хедере. Это должны быть соседи в имени класса которых есть two-line
+        # поэтому получаем соседей. Итерируемся по ним. Как -только получаем ничего или соседа без two-line
+        # значит матчи в топ-хедере закончились
+
+        top_header_siblings = top_header.find_elements("xpath", "//div[@class='event__header top']/following-sibling::*")
+        # получаем список id табличек, которые относятся к топ-хедеру
+
+        list_of_tables_in_top_header = []
+        for block in top_header_siblings:
+            class_name = block.get_attribute("class")
+            if "twoLine" in class_name:
+                id = block.get_attribute("id")
+                list_of_tables_in_top_header.append(id)
+                top_header_data[top_header_league_id] = list_of_tables_in_top_header
+
+        print("Количество матчей в топ-хедере: ", len(list_of_tables_in_top_header))
+
 
 
     except Exception as e:
-        print(e)
+        print("Исключение возникло при попытке получить топ-хедер. ", e)
+        return redirect(url_for('home.home_view'))
 
-    # все нижние соседи
+    # получаем список обычных хедеров
+    try:
+        event_headers = driver.find_elements("xpath", "//div[@class='event__header']")
+        for event_header in event_headers:
+            country_event_header = event_header.find_element("xpath", "//span[@class='event__title--type']")
+            league_event_header = event_header.find_element("xpath", "//span[@class='event__title--name']")
+            country_event_header = country_event_header.text.lower()
+            league_event_header = league_event_header.text
+            print("event_header. Страна: ", country_event_header, ", лига: ", league_event_header)
 
-    siblings = top_header.find_elements("xpath", "//div[@class='event__header top']/following-sibling::*")
-    for block in siblings:
-        try:
-            class_name = block.get_attribute("class")
-            # print("class_name: ", class_name)
-        except:
-            pass
+            # проверяем есть ли данные о лиге и командах. Если нет, то создаем
+            update_league_and_team_data(driver, country_event_header, league_event_header, sport_id)
 
-        try:
-            id = block.get_attribute("id")
-            # print(id)
-        except:
-            pass
+    except Exception as e:
+        print("Исключение возникло при попытке получить список обычных хедеров. ", e)
+        return redirect(url_for('home.home_view'))
 
-    return "парсер живых матчей"
+
+
+
+    return top_header_data
 
 
 @home.route('/parse_league')
 def parse_league():
     driver = webdriver.Chrome("/usr/local/bin/chromedriver")
     # web_page = "https://www.flashscorekz.com/basketball/argentina/liga-a/standings/"
-    web_page = "https://www.flashscorekz.com/basketball/europe/champions-league/standings/"
+    # web_page = "https://www.flashscorekz.com/basketball/europe/champions-league/standings/"
+    web_page = "https://www.flashscorekz.com/basketball/usa/nba/standings/"
+
     driver.get(web_page)
     driver.implicitly_wait(10)
     # жмем на кнопку принять куки.
